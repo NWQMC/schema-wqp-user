f74a9e845d9c881ae1a59e1a30ef43f7dd95e25c
Dave Steinich <drsteini@usgs.gov>
2014-03-14 20:54:39 +0000

    incremental liquibase script

--- initialConfig/initialConfigSynonyms.sql
+++ initialConfig/initialConfigSynonyms.sql
@@ -9,3 +9,7 @@ create or replace synonym storet_fa_regular_result for storetmodern.fa_regular_r
 --changeset kmschoep:nwis_result_syn
 create or replace synonym nwis_fa_regular_result for nwis_ws_star.fa_regular_result;
 --rollback drop synonym nwis_fa_regular_result;
+
+--changeset drsteini:1SchemaSynonymsAC
+create or replace synonym web_service_log for wqp_core.web_service_log;
+--rollback drop synonym web_service_log;
